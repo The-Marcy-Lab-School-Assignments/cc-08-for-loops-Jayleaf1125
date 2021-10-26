df7a592db21bc840fbef4b14ab863012cc1345c8
Jayleaf1125 <Jasonpadilla104@gmail.com>
2021-10-26 14:33:55 +0000

    Code Challenge 08 completed

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
 //1
-function countToTwenty(){
-  
+function countToTwenty() {
+    for (let i = 1; i <= 20; i++) {
+        console.log(i);
+    }
 }
 
 //2
-function countFromTwenty(){
-  
+function countFromTwenty() {
+    for(let i = 20; i >= 1; i--) {
+      console.log(i);
+  }
 }
 
 //3
-function countToN(){
-  
+function countToN(int) {
+    for(let i = 1; i <= int; i++){
+        console.log(i);
+    }
 }
 
 
 //4
-function countFromN(){
-  
+function countFromN(num) {
+    for(let i = num; i >= 1; i--) {
+        console.log(i);
+    }
+
 }
 
 //5
-function countEveryOdd(){
-  
+function countEveryOdd(int) {
+    for(let i = 1; i <= int; i += 2) {
+        console.log(i);
+    }
+
 }
+
+// Call
+// countToTwenty();
+// countFromTwenty();
+// countToN(10);
+// countFromN(10);
+// countEveryOdd(10);
